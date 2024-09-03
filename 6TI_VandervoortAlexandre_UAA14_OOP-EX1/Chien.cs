0431ebae69c27135c6b73ed678ab611e07892965
Vandervoort Alexandre <220857@CCM-Asty-Moulin.local>
2024-09-03 11:49:00 +0200

    Exercice OOP chien

---
+++ 6TI_VandervoortAlexandre_UAA14_OOP-EX1/Chien.cs
@@ -0,0 +1,89 @@
+﻿namespace _6TI_VandervoortAlexandre_UAA14_OOP_EX1
+{
+    internal class Chien
+    {
+        private ushort _age { get; set; }
+        private string _race { get; set; }
+        private double _taille { get; set; }
+        private double _poid { get; set; }
+        private string _etat { get; set; }
+        private bool _puce { get; set; }
+
+        private bool nourrit = false;
+        private bool aBu = false;
+
+        private static Random randomGen = new Random();
+
+        public Chien(string race, double taille, double poid, string etat, bool puce)
+        {
+            _age = 0;
+            _race = race;
+            _taille = taille;
+            _poid = poid;
+            _etat = etat;
+            _puce = puce;
+        }
+
+        public string Manger()
+        {
+            if (nourrit == false)
+            {
+                nourrit = true;
+            }
+
+            return null;
+        }
+
+        public void Vieillir()
+        {
+            if (!nourrit || !aBu)
+            {
+                if (_etat == "Affamé")
+                {
+                    Mourrir();
+                } else
+                {
+                    _etat = "Affamé";
+                }
+                return;
+            }
+
+            _age++;
+            _taille += randomGen.NextDouble();
+            _poid += randomGen.NextDouble() * 10;
+            nourrit = false;
+            aBu = false;
+        }
+
+        public string Boire()
+        {
+            if (aBu == false)
+            {
+                aBu = true;
+            }
+            return null;
+        }
+
+        public void Blesser()
+        {
+            if (_etat != "Mort" && _etat != "Blessé")
+            {
+                _etat = "Blessé";
+            }
+        }
+
+        public void Mourrir()
+        {
+            if (_etat != "Mort")
+            {
+                _etat = "Mort";
+                _race = "Os";
+            }
+        }
+
+        public override string ToString()
+        {
+            return "{" + "age: " + _age + ", race: " + _race + ", taille: " + _taille + "m, poid: " + _poid + "kg, etat: " + _etat + ", pucé: " + _puce + "}";
+        }
+    }
+}
---
+++ 6TI_VandervoortAlexandre_UAA14_OOP-EX1/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _6TI_VandervoortAlexandre_UAA14_OOP_EX1
+{
+    internal class Program
+    {
+        public static string[] races = new string[]
+        {
+            "Golden Retriever",
+            "Bichon",
+            "Bulldog",
+            "Pug",
+            "Shiba Inu"
+        };
+
+        static void Main(string[] args)
+        {
+            ConsoleKey entree = ConsoleKey.NumPad0;
+            while (entree != ConsoleKey.NumPad4)
+            {
+                Chien chien = new(races[Random.Shared.Next(races.Length - 1)], Random.Shared.NextDouble(), Random.Shared.NextDouble() * 10, "Bonne Santé", false);
+
+                Console.WriteLine("Nouveau chien!!:\n" + chien);
+
+                while (entree != ConsoleKey.NumPad4)
+                {
+                    Console.WriteLine("Que voulez vous faire? Numpad1: Nourrir Le chien, Numpad2: Donner à boire au chien, Numpad3: Faire vieillir le chien, Numpad4: Abandonner le chien");
+                    entree = Console.ReadKey(true).Key;
+                    while (entree != ConsoleKey.NumPad1 && entree != ConsoleKey.NumPad2 && entree != ConsoleKey.NumPad3 && entree != ConsoleKey.NumPad4)
+                    {
+                        Console.WriteLine("J'ai pas compris ce que vous vouilliez faire");
+                        entree = Console.ReadKey(true).Key;
+                    }
+
+                    if (entree == ConsoleKey.NumPad1)
+                    {
+                        chien.Manger();
+                        Console.WriteLine("Vous avez nourrit votre chien.");
+                    } else if (entree == ConsoleKey.NumPad2)
+                    {
+                        chien.Boire();
+                        Console.WriteLine("Vous avez donné de l'eau à votre chien.");
+                    } else if (entree == ConsoleKey.NumPad3)
+                    {
+                        chien.Vieillir();
+                        Console.WriteLine("Votre chien à vieilli");
+                    } else if (entree == ConsoleKey.NumPad4)
+                    {
+                        Console.WriteLine("Vous venez d'abandonner ce chien. :(");
+                        break;
+                    }
+                    Console.WriteLine("Votre chien:\n" + chien);
+                }
+
+
+            }
+        }
+    }
+}
