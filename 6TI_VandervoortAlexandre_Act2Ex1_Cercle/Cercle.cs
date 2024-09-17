d88672d9eeffca9d17d3bcb8888215343c640f55
Vandervoort Alexandre <220857@CCM-Asty-Moulin.local>
2024-09-17 11:49:49 +0200

    début act 2 ex 1 (cercle)

---
+++ 6TI_VandervoortAlexandre_Act2Ex1_Cercle/Cercle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6TI_VandervoortAlexandre_Act2Ex1_Cercle
+{
+    internal class Cercle
+    {
+        private double _rayon;
+
+        public double Rayon
+        {
+            get { return _rayon; }
+            set { _rayon = value; }
+        }
+
+        public Cercle(double rayon)
+        {
+            _rayon = rayon;
+        }
+
+        public double CalculeAire()
+        {
+            return Math.PI * Math.Pow(_rayon, 2);
+        }
+
+        public double CalculePerimetre()
+        {
+            return Math.PI * (2 * _rayon);
+        }
+    }
+}
---
+++ 6TI_VandervoortAlexandre_Act2Ex1_Cercle/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _6TI_VandervoortAlexandre_Act2Ex1_Cercle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue dans ce programme sur le cercle !");
+            
+            bool continuer = true;
+            while (continuer)
+            {
+                double rayon = QuestionneUtilisateurDouble(("Taper un rayon pour votre cercle.");
+                Cercle cercle = new Cercle(rayon);
+            }
+        }
+
+        public static String QuestionneUtilisateur(String question, Func<String, bool> condition)
+        {
+            Console.WriteLine(question);
+            string res = Console.ReadLine();
+
+            while (res == null || !condition.Invoke(res))
+            {
+                Console.WriteLine("Je n'ai pas compris ce que vous avez dis.");
+                res = Console.ReadLine();
+            }
+
+            return res;
+        }
+
+        public static double QuestionneUtilisateurDouble(String question)
+        {
+            return Double.Parse(QuestionneUtilisateur(question, (str) => Int32.TryParse(str, out int val)));
+        }
+    }
+}
