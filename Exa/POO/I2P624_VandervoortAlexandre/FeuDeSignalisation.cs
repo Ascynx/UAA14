9bfb571c85c0b439df0bc31e55d48abab0cb9546
Vandervoort Alexandre <220857@CCM-Asty-Moulin.local>
2024-10-15 11:05:01 +0200

    interro POO 2$

---
+++ Exa/POO/I2P624_VandervoortAlexandre/FeuDeSignalisation.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I2P624_VandervoortAlexandre
+{
+    internal class FeuDeSignalisation
+    {
+        private int _couleur;
+        private readonly string _identifiant;
+        private bool _etat;
+
+        public int Couleur { get { return _couleur; } set { _couleur = value; } }
+        public bool Etat { get { return _etat; } set { _etat = value; } }
+
+        public FeuDeSignalisation(int couleur, string identifiant, bool etat)
+        {
+            _couleur = couleur;
+            _identifiant = identifiant;
+            _etat = etat;
+        }
+
+        public int ChangeCouleur(int couleur)
+        {
+            _couleur = couleur;
+            return _couleur;
+        }
+
+        public void ChangeEtat()
+        {
+            _etat = !_etat;
+        }
+
+        public void Clignote(int nbAlternance)
+        {
+            Console.WriteLine("Le feu de signalisation " + _identifiant + " est " + (Etat ? "allumé" : "éteint"));
+            for (int i = 0; i < nbAlternance; i++)
+            {
+                ChangeEtat();
+                Console.WriteLine("Le feu de signalisation " + _identifiant + " est " + (Etat ? "allumé" : "éteint") + ".");
+            }
+        }
+
+        public string AfficheCouleur()
+        {
+            return "Le feu de signalisation " + _identifiant + " est " + GetStringColorRepresentation(_couleur);
+        }
+
+        public string AfficheEtat()
+        {
+            return "Le feu de signalisation " + _identifiant + " est " + GetStringColorRepresentation(_couleur) + " et " + (Etat ? "allumé" : "éteint") + ".";
+        }
+
+        static string GetStringColorRepresentation(int couleur)
+        {
+            switch (couleur)
+            {
+                case 0:
+                    {
+                        return "Vert";
+                    }
+                case 1:
+                    {
+                        return "Orange";
+                    }
+                case 2:
+                    {
+                        return "Rouge";
+                    }
+                default:
+                    {
+                        return "UNKNOWN";
+                    }
+            }
+        }
+    }
+}
---
+++ Exa/POO/I2P624_VandervoortAlexandre/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace I2P624_VandervoortAlexandre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FeuDeSignalisation[] feux = new FeuDeSignalisation[2];
+
+            feux[0] = new FeuDeSignalisation(0, "1001", false);
+            feux[1] = new FeuDeSignalisation(0, "007", false);
+
+            Console.WriteLine("\nEtat et couleurs des feux : ");
+            Console.WriteLine("-----------------------------------");
+            
+            Console.WriteLine(feux[0].AfficheEtat());
+            Console.WriteLine(feux[1].AfficheEtat());
+
+
+            //passe l'état
+            feux[0].ChangeEtat();
+            feux[1].ChangeEtat();
+
+
+            Console.WriteLine("\nFaire passer le 007 à l'orange : ");
+            Console.WriteLine("-----------------------------------");
+
+            feux[1].ChangeCouleur(1);
+            Console.WriteLine(feux[1].AfficheEtat());
+
+
+            Console.WriteLine("\nFeu clignotant : ");
+            Console.WriteLine("-----------------------------------");
+
+            feux[1].Clignote(5);
+
+
+            Console.WriteLine("\nChangement d'état : ");
+            Console.WriteLine("-----------------------------------");
+
+            for (int i = 0; i < 5; i++)
+            {
+                int x = 5-(i+3);
+                if (x < 0)
+                {
+                    x = 3 + x;
+                }
+
+                feux[0].ChangeCouleur(x); //2, 1, 0, 2, 1
+                Console.WriteLine(feux[0].AfficheCouleur());
+            }
+        }
+    }
+}
