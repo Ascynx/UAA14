c2c7a9bae2ef094d7a2048e9ada97b3d4771b381
Vandervoort Alexandre <220857@CCM-Asty-Moulin.local>
2024-11-07 14:27:00 +0100

    WPF Act 4 fenètre multi-page

---
+++ WPF/6TI_VandervoortAlexandre_WPF_Act4_MultiPageWindow/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _6TI_VandervoortAlexandre_WPF_Act4_MultiPageWindow.Vues;
+
+namespace _6TI_VandervoortAlexandre_WPF_Act4_MultiPageWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Page1Button.Click += onPage1ButtonPress;
+            Page2Button.Click += onPage2ButtonPress;
+        }
+
+        public void onPage1ButtonPress(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new PageOne();
+        }
+
+        public void onPage2ButtonPress(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new PageTwo();
+        }
+    }
+}
