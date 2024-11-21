91b6c5e9169d17358bb5bb91b8de8aa062675ba8
Vandervoort Alexandre <220857@CCM-Asty-Moulin.local>
2024-11-21 16:08:01 +0100

    WPF Act 6 Damiers (début)

---
+++ WPF/6TI_VA_WPF_Act6_Damiers/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _6TI_VA_WPF_Act6_Damiers
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ WPF/6TI_VA_WPF_Act6_Damiers/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _6TI_VA_WPF_Act6_Damiers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Width = 660;
+            this.Height = 660;
+            InitializeDynamicComponents();
+        }
+        
+        public void InitializeDynamicComponents()
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    RowDefinition row = new RowDefinition()
+                    {
+                        Height = new GridLength(65),
+                    };
+                    ColumnDefinition column = new ColumnDefinition()
+                    {
+                        Width = new GridLength(65)
+                    };
+
+                    damier.ColumnDefinitions.Add(column);
+                    damier.RowDefinitions.Add(row);
+
+                    int colN = 10 * x;
+                    int rowN = y;
+
+                    string num;
+                    if ((x + 1) % 2 == 0)
+                    {
+                        num = "" + ((colN + 10) - rowN);
+                    } else
+                    {
+                        num = "" + (colN + rowN);
+                    }
+
+                    TextBlock block = new TextBlock()
+                    {
+                        Text = num,
+                        FontSize = 36,
+                        Foreground = Brushes.Red,
+                        Background = (y % 2 == 0) == (x % 2 == 0) ? Brushes.White : Brushes.Black,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                    };
+                    Grid.SetRow(block, x);
+                    Grid.SetColumn(block, y);
+
+                    damier.Children.Add(block);
+                }
+            }
+        }
+    }
+}
