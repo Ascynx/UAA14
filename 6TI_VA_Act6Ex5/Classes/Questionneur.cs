95291a70d0e91d79b1f08140ee7c634b01892a2e
Ascynx <miaoum2128@gmail.com>
2024-11-26 06:52:00 +0100

    Act 6 Exercice 5

---
+++ 6TI_VA_Act6Ex5/Classes/Questionneur.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6TI_VA_Act6Ex5.Classes
+{
+    internal class Questionneur
+    {
+        public static String QuestionneUtilisateur(String question, Func<String, bool> condition)
+        {
+            Console.WriteLine(question);
+            string res = Console.ReadLine();
+
+            while (res == null || !condition.Invoke(res))
+            {
+                Console.WriteLine("Je n'ai pas compris ce que vous avez dit.");
+                res = Console.ReadLine();
+            }
+
+            return res;
+        }
+
+        public static int QuestionneUtilisateurIntMinMax(string question, int min, int max)
+        {
+            return Int32.Parse(QuestionneUtilisateur(question, (str) => Int32.TryParse(str, out int val) && (val >= min && val <= max)));
+        }
+        
+        public static int QuestionneUtilisateurInt(string question)
+        {
+            return Int32.Parse(QuestionneUtilisateur(question, (str) => Int32.TryParse(str, out int val)));
+        }
+
+        public static double QuestionneUtilisateurDouble(String question)
+        {
+            return Double.Parse(QuestionneUtilisateur(question, (str) => Double.TryParse(str, out double val)));
+        }
+
+        public static string QuestionneUtilisateurSimple(String question)
+        {
+            return QuestionneUtilisateur(question, (str) => true);
+        }
+
+        public static bool QuestionneUtilisateurYNQuestion(String question) {
+            return new char[]{'y', 'o'}.Contains(QuestionneUtilisateur(question, (str) => {
+                string lowerStr = str.ToLower();
+                
+                //oui: y | yes | oui | o
+                //non: n | no | non
+                return lowerStr == "y" || lowerStr == "n" || lowerStr == "yes" || lowerStr == "no" || lowerStr == "oui" || lowerStr == "non" || lowerStr == "o";
+            }).First());
+        }
+    }
+}
--- 6TI_VA_Act6Ex5/Program.cs
+++ 6TI_VA_Act6Ex5/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace _6TI_VA_Act6Ex5
+﻿using System.Reflection.Emit;
+using _6TI_VA_Act6Ex5.Classes;
+
+namespace _6TI_VA_Act6Ex5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            System.Console.WriteLine("Bienvenue dans le programme concessionnaire.");
+
+            bool continuer = true;
+            while (continuer) {
+                int choix = Questionneur.QuestionneUtilisateurIntMinMax("Quel type de véhicule voulez vous?\n1) Bateau\n2) Voiture\n3) Avion\n4) Camion.", 1, 4);
+                Vehicle? vehicule = ChoiceToVehicle(choix);
+                if (vehicule == null) {
+                    System.Console.WriteLine("Erreur, je n'ai pas compris votre choix.");
+                    break;
+                }
+
+                if (vehicule is Boat boat) {
+                    System.Console.WriteLine(boat);
+                } else if (vehicule is Car car) {
+                    System.Console.WriteLine(car);
+                } else if (vehicule is Plane plane) {
+                    System.Console.WriteLine(plane);
+                } else if (vehicule is Truck truck) {
+                    System.Console.WriteLine(truck);
+                }
+
+                continuer = Questionneur.QuestionneUtilisateurYNQuestion("Voulez vous continuer? y: oui, n: non");
+            }
+        }
+
+        static Vehicle? ChoiceToVehicle(int choix) {
+            return choix switch
+            {
+                1 => new Boat("boat company", 0.0F, 60D),
+                2 => new Car("car company", 0.0F, 60000.05D),
+                3 => new Plane("plane company", 0.0F, 600D),
+                4 => new Truck("truck company", 0.0F, 30000D, 6000D),
+                _ => null,
+            };
         }
     }
 }
